88231c12022ed3883cc07dbf1d0934e80b4c652d
MetalTurtle <dekolis1218@gmail.com>
2020-09-15 10:27:45 -0700

    Create method binaryToDecimal

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,13 +2,20 @@ import java.util.Scanner;
 
 public class MyMain {
     
-    public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    public static int binaryToDecimal(String binary) {
+        int output = 0;
+        for (int i = binary.length()-1; i >= 0; i--) {
+            output += Integer.parseInt(binary.substring(i, i+1)) * Math.pow(2, binary.length()-1-i);
+        }
+        return output;
+        // But also why not just use this: return Integer.parseInt(binary, 2);
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format: ");
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(scan.next()));
+        scan.close();
     }
 }
